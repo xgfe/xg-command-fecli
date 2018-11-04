e14059964883ce61a386d348a22820516377e6ff
周雄 <zhouxiong03@meituan.com>
2018-11-04 11:53:14 +0800

    chore(all) npm init

---
+++ index.js
@@ -0,0 +1,24 @@
+const klcli = require('kxcli');
+const version = require('./package.json').version;
+
+exports.name = 'kxcli';
+
+exports.desc = 'xg kxcli plugin, using kxcli';
+
+exports.options = {
+    '-v, --version': 'version',
+    '-m': 'create new module',
+    '-h, --help': 'print this help message'
+};
+
+exports.run = function (argv, cli, env) {
+    // 显示帮助信息
+    if(argv.h || argv.help) {
+        return cli.help(exports.name, exports.options);
+    }
+
+    if(argv.v || argv.version) {
+        console.log(`kxcli verson ${kxcli.version}`);
+        console.log(`xg-command-kxcli version ${version}`);
+    }
+};
